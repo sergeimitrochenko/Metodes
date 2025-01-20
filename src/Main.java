3aac35b06eac91b6da291fa7572a5cf107be6058
sergeimitrochenko <aron.armenyan@yandex.ru>
2025-01-20 15:42:07 +0300

    "Продолжение проекта:задача №1,задача №2,задача №3"

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         years(year);
         System.out.println();
     }
+
     public static void years(int year) {
         if (year < 1584) {
             System.out.println(year + " год не является високосным");
@@ -21,6 +22,7 @@ public class Main {
         OSAndClientDeviceYear(OS, clientDeviceYear);
         System.out.println();
     }
+
     public static void OSAndClientDeviceYear(int OS, int clientDeviceYear) {
         if (OS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
@@ -30,6 +32,21 @@ public class Main {
             System.out.println("Установите обычную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        System.out.println("Задача № 3");
+        int deliveryDistance = distance(95);
+        System.out.println(deliveryDistance);
+    }
+
+    public static int distance(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            return 1;
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            return 2;
+        } else if (deliveryDistance > 60 && deliveryDistance < 100) {
+            return 3;
+        } else {
+            return -1;
         }
     }
 }
