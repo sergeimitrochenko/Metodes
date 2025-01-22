491669ee7207e6b79f5960903c44aaa2311a9270
sergeimitrochenko <aron.armenyan@yandex.ru>
2025-01-22 11:52:24 +0300

    "Задача №1 №2 №3"

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@ public class Main {
         int year = 2024;
         years(year);
         System.out.println();
+        System.out.println("Задача № 2");
+        int clientDeviceYear = 2013;
+        int OS = 0;
+        OSAndClientDeviceYear(OS, clientDeviceYear);
+        System.out.println();
+        System.out.println("Задача № 3");
+        int deliveryDistance = distance(95);
+        System.out.println(deliveryDistance);
     }
 
     public static void years(int year) {
@@ -14,6 +22,29 @@ public class Main {
             System.out.println(year + "год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void OSAndClientDeviceYear(int OS, int clientDeviceYear) {
+        if (OS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (OS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (OS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+    public static int distance(int deliveryDistance){
+        if (deliveryDistance < 20) {
+            return 1;
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            return 2;
+        } else if (deliveryDistance > 60 && deliveryDistance < 100) {
+            return 3;
+        } else {
+            return -1;
         }
     }
 }
